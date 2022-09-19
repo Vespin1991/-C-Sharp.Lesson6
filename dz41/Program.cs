49f464381fe0bb07e96570979f6dc3c393a46a8f
Aleksandr <Vespin1991@bk.ru>
2022-09-19 22:04:51 +0300

    dz41

---
+++ dz41/Program.cs
@@ -0,0 +1,14 @@
+﻿//  Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+Console.WriteLine("Введите M");
+int M = Convert.ToInt32(Console.ReadLine());
+int[] massiv = new int[M];
+int count = 0; 
+    for (int i = 0; i < M; i++)
+{
+    Console.Write("Введите число   ");
+    massiv[i] = Convert.ToInt32(Console.ReadLine()); 
+    
+              if (massiv[i]>0)
+               {count = count + 1;
+}  }
+Console.WriteLine($"Колличество чисел больше нуля равно {count}");
